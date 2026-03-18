74d822e2f3bbbf4d628f0ab6f77dfaa691bfda34
Jean <jeanponte2015@gmail.com>
2026-03-18 10:28:15 -0300

    Add novas funcionalidades

--- App_site/models.py
+++ App_site/models.py
@@ -18,18 +18,22 @@ def criar_tabela_usuarios(): #Usado para criar tabela usuário (Chamada apenas u
         CREATE TABLE IF NOT EXISTS usuarios (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             username TEXT NOT NULL UNIQUE,
-            password TEXT NOT NULL
+            password TEXT NOT NULL,
+            role TEXT NOT NULL DEFAULT 'user'       
         )
     ''')
     conn.commit()
     conn.close()
 
-def adicionar_usuario(username, password): #Usado para adicionar usuário
+def adicionar_usuario(username, password,role='user'): #Usado para adicionar usuário
     try:
         conn = conectar_user_db()
         cursor = conn.cursor()
         hashed_password = generate_password_hash(password)
-        cursor.execute('INSERT INTO usuarios (username, password) VALUES (?, ?)', (username, hashed_password))
+        cursor.execute(
+            'INSERT INTO usuarios (username, password, role) VALUES (?, ?, ?)',
+            (username, hashed_password, role)
+            )
         conn.commit()
     except sqlite3.IntegrityError as e:
         print(f"Erro ao adicionar usuário: {e}")
@@ -62,7 +66,7 @@ def listar_tabelas(): #Usado para listar as tabelas que aparecerão no Dropdown 
 
 def ler_tabela(tabela): #Mostra a informações da tabela selecionada
     conn = conectar_estoque_db()
-    df = pd.read_sql_query(f"SELECT * FROM {tabela} LIMIT 100", conn)
+    df = pd.read_sql_query(f"SELECT * FROM {tabela}", conn)
     conn.close()
     return df
 
@@ -88,8 +92,8 @@ def quant_no_bd(id): #Usado para mostrar quantidades de itens com mesmo ID bando
 def registrar_movimentacao_por_qr(qr_completo, produto_id, observacao=""):
     with conectar_estoque_db() as conn:
         cursor = conn.cursor()
+        agora = datetime.now()
 
-        # Verifica se já existe entrada
         cursor.execute("""
             SELECT data_movimentacao FROM movimentacoes
             WHERE qr_unico = ? AND tipo = 'entrada'
@@ -98,26 +102,22 @@ def registrar_movimentacao_por_qr(qr_completo, produto_id, observacao=""):
         """, (qr_completo,))
         ultima_entrada = cursor.fetchone()
 
-        # Verifica se já existe saída
         cursor.execute("""
             SELECT 1 FROM movimentacoes
             WHERE qr_unico = ? AND tipo = 'saida'
         """, (qr_completo,))
         tem_saida = cursor.fetchone() is not None
 
-        agora = datetime.now()
-
         if not ultima_entrada:
             tipo = "entrada"
 
-        elif ultima_entrada and not tem_saida:
+        elif not tem_saida:
             data_entrada = datetime.strptime(ultima_entrada[0], "%Y-%m-%d %H:%M:%S")
 
-            #Bloqueia saída se não passou 1 minuto da entrada
             if agora - data_entrada < timedelta(minutes=1):
                 return {
                     "status": "erro",
-                    "mensagem": "Saida bloqueada: leitura muito prixima da entrada (aguarde 1 minuto)."
+                    "mensagem": "Saída bloqueada: aguarde 1 minuto da entrada."
                 }
 
             tipo = "saida"
@@ -125,20 +125,55 @@ def registrar_movimentacao_por_qr(qr_completo, produto_id, observacao=""):
         else:
             return {
                 "status": "erro",
-                "mensagem": "Este item já possui saída registrada."
+                "mensagem": "Item já possui saída."
             }
 
         try:
+            # garante registro no estoque
             cursor.execute("""
-                INSERT INTO movimentacoes (produto_id, tipo, data_movimentacao, observacao, qr_unico)
-                VALUES (?, ?, datetime('now','localtime'), ?, ?)
-            """, (produto_id, tipo, observacao, qr_completo))
+                INSERT OR IGNORE INTO estoque (produto_id, quantidade)
+                VALUES (?, 0)
+            """, (produto_id,))
+
+            # registra movimentação
+            cursor.execute("""
+                INSERT INTO movimentacoes (
+                    produto_id, tipo, data_movimentacao,
+                    observacao, qr_unico, quantidade
+                )
+                VALUES (?, ?, datetime('now','localtime'), ?, ?, ?)
+            """, (produto_id, tipo, observacao, qr_completo, 1))
+
+            if tipo == "entrada":
+
+                cursor.execute("""
+                    UPDATE estoque
+                    SET quantidade = quantidade + 1
+                    WHERE produto_id = ?
+                """, (produto_id,))
+
+            else:  # saída
+
+                cursor.execute("""
+                    UPDATE estoque
+                    SET quantidade = quantidade - 1
+                    WHERE produto_id = ? AND quantidade > 0
+                """, (produto_id,))
+
+                if cursor.rowcount == 0:
+                    conn.rollback()
+                    return {
+                        "status": "erro",
+                        "mensagem": "Estoque insuficiente."
+                    }
+
             conn.commit()
 
         except sqlite3.IntegrityError:
+            conn.rollback()
             return {
                 "status": "erro",
-                "mensagem": "Movimentação duplicada detectada."
+                "mensagem": "Movimentação duplicada."
             }
 
         return {
@@ -147,7 +182,6 @@ def registrar_movimentacao_por_qr(qr_completo, produto_id, observacao=""):
             "qr": qr_completo
         }
 
-        
 def buscar_produto_por_codigo_ou_nome(query):
     conn = conectar_estoque_db()
     conn.row_factory = sqlite3.Row
@@ -160,6 +194,136 @@ def buscar_produto_por_codigo_ou_nome(query):
     produto = cursor.fetchone()
     conn.close()
     return produto
+
+TABELAS_PERMITIDAS = {
+    "produtos",
+    "estoque",
+    "movimentacoes",
+    "usuarios"
+}
+
+def buscar_tabela_db(tabela, termo=""):
+
+    conn = conectar_estoque_db()
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+    
+    base_query = f"SELECT * FROM {tabela}"
+    valores = []
+
+    if termo:
+        cursor.execute(f"PRAGMA table_info({tabela})")
+        colunas = cursor.fetchall()
+
+        colunas_texto = [
+            col[1] for col in colunas
+            if col[2] and "TEXT" in col[2].upper()
+        ]
+
+        if colunas_texto:
+            filtros = " OR ".join([f"{col} LIKE ?" for col in colunas_texto])
+            base_query += f" WHERE {filtros}"
+            valores = [f"%{termo}%"] * len(colunas_texto)
+
+    if tabela == 'pedidos':
+        base_query += " ORDER BY data_registro DESC"
+
+    cursor.execute(base_query, valores)
+
+    dados = cursor.fetchall()
+    conn.close()
+    return dados
+
+def estoque_view():
+    conn = conectar_estoque_db()
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+    
+    cursor.execute("SELECT * from estoque_legivel;")
+    dados = cursor.fetchall()
+    
+    conn.close()
+    return dados
+
+def ordens_view():
+    conn = conectar_estoque_db()
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+    
+    cursor.execute("""SELECT * 
+                    FROM pedidos 
+                    ORDER BY data_registro DESC;""")
+    dados = cursor.fetchall()
+    
+    conn.close()
+    return dados
+
+
+def registrar_pedido(qr, volume, quem, quantidade_editada, status):
+
+    partes = qr.split("|")
+
+    if len(partes) != 7:
+        return {"erro": "QR inválido"}
+
+    data_pedido, nPedido , entrega, produto, qtd, comprimento, cliente = partes
+    
+    try:
+        if quantidade_editada:
+            quantidade_final = int(quantidade_editada)
+        else:
+            quantidade_final = int(qtd)
+    except ValueError:
+        quantidade_final = 0
+
+    volume_final = float(volume) if volume else None
+
+    with conectar_estoque_db() as conn:
+        cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT COUNT(*) FROM pedidos WHERE numero_pedido LIKE ?",
+        (f"{nPedido}%",)
+    )
+
+    contador = cursor.fetchone()[0]
+
+    novo_numero = nPedido if contador == 0 else f"{nPedido}/{contador}"
+
+    cursor.execute("""
+        INSERT INTO pedidos (
+            numero_pedido,
+            produto,
+            quantidade,
+            comprimento,
+            volume,
+            cliente,
+            data_pedido,
+            entrega,
+            usuario,
+            status,
+            data_registro
+        )
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    """, (
+        novo_numero,
+        produto,
+        quantidade_final,
+        comprimento,
+        volume_final,
+        cliente,
+        data_pedido,
+        entrega,
+        quem,
+        status,
+        datetime.now()
+    ))
+
+    conn.commit()
+    conn.close()
+
+    return {"msg": f"Pedido {novo_numero} registrado com sucesso"}
+
 
 #Apesar de não ser Banco de dados botei a função no models
 #Serve para mudar o arquivo input da etiqueta. Mudar ela, muda o qrcode impressa na etiqueta 
--- App_site/views.py
+++ App_site/views.py
@@ -4,7 +4,9 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 from functools import wraps
 from models import (buscar_usuario, listar_tabelas, ler_tabela,
                      buscar_produto_por_qr, registrar_movimentacao_por_qr,
-                     buscar_produto_por_codigo_ou_nome,mudar_qr,quant_no_bd)
+                     buscar_produto_por_codigo_ou_nome,mudar_qr,
+                     quant_no_bd,buscar_tabela_db,estoque_view,ordens_view,registrar_pedido)
+from datetime import datetime
 from werkzeug.security import check_password_hash
 
 app = Flask(__name__)
@@ -19,6 +21,23 @@ def login_required(f):
             return redirect(url_for('login'))
         return f(*args, **kwargs)
     return decorated_function
+
+def role_required(*roles):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+
+            if 'user' not in session:
+                flash('Você precisa estar logado.')
+                return redirect(url_for('login'))
+
+            if session['user'].get('role') not in roles:
+                return "Acesso negado", 403
+
+            return f(*args, **kwargs)
+
+        return decorated_function
+    return decorator
 
 @app.route('/')
 def index():
@@ -39,7 +58,7 @@ def login():
 
         usuario = buscar_usuario(username)
         if usuario and check_password_hash(usuario[2], password):
-            session['user'] = {'id': usuario[0], 'username': usuario[1]}
+            session['user'] = {'id': usuario[0], 'username': usuario[1],'role': usuario[3]}
             session.permanent = False
             return redirect(url_for('homepage'))
         else:
@@ -52,11 +71,12 @@ def logout():
     return redirect(url_for('login'))
 
 @app.route('/formulario')
-@login_required
+@role_required('admin')
 def formulario():
     return render_template('form.html')
 
 @app.route('/buscar_produto')
+@login_required
 def buscar_produto():
     query = request.args.get('q', '').strip()
     if not query:
@@ -77,16 +97,21 @@ def buscar_produto():
 @app.route('/carregar_tabela/<tabela>')
 @login_required
 def carregar_tabela(tabela):
+
     tabelas_validas = listar_tabelas()
     if tabela not in tabelas_validas:
         return f'<p class="text-danger">Tabela "{tabela}" não encontrada.</p>', 404
 
-    df = ler_tabela(tabela)
+    dados = buscar_tabela_db(tabela)
 
-    tabela_html = df.to_html(classes='data table table-bordered', index=False)
-    return tabela_html
+    return render_template(
+        "partials/tabela.html",
+        dados=dados,
+        tabela=tabela
+    )
 
 @app.route('/mudar_qrcode/<int:id>/<qr>') #Alterar arquivo input_qr
+@role_required('admin')
 @login_required
 def mudar_input_qr(id,qr):
     quantidade_linhas = int(quant_no_bd(id))
@@ -97,17 +122,18 @@ def mudar_input_qr(id,qr):
     return qr
 
 @app.route('/dashboard')
-@login_required
+@role_required('admin')
 def dashboard():
     tabelas = listar_tabelas()
     return render_template('dashboard.html', tabelas=tabelas)
 
 @app.route('/etiqueta')
-@login_required
+@role_required('admin')
 def etiqueta():
     return render_template('etiquetas.html')
 
 @app.route('/abrir_exe', methods=['POST'])
+@role_required('admin')
 def abrir_exe():
     bartender = r"C:/Program Files (x86)/Seagull/BarTender Suite/bartend.exe" #Caminho do Bartender 
     arquivo_btw = r"C:/Users/Jnpx_/Desktop/33x22 qrcode.btw" #Caminho da etiqueta
@@ -118,11 +144,13 @@ def abrir_exe():
         return jsonify({"status": str(e)}), 500
     
 @app.route('/movimentacao')
+@role_required('admin')
 def movimentacao():
     return render_template('movimentacao.html')
 
 #Retirei o @Login_required de algumas route pois dava erro, não é seguro fazer isso. Vou consertar depois 
 @app.route('/movimentacao/<qr>/<id_unico>')
+@role_required('admin')
 def registrar_movimentacao(qr, id_unico):
     qr_completo = f"{qr}/{id_unico}"
 
@@ -144,4 +172,88 @@ def registrar_movimentacao(qr, id_unico):
         return jsonify(resultado), 400
 
     return jsonify(resultado)
-    
+
+@app.route("/buscar_tabela/<tabela>")
+def buscar_ajax(tabela):
+    termo = request.args.get("termo", "")
+    dados = buscar_tabela_db(tabela, termo)
+
+    if tabela == "pedidos":
+        return render_template(
+            "partials/tabela_pedidos.html",
+            pedidos=dados
+        )
+    else:
+        return render_template(
+            "partials/tabela.html",
+            dados=dados,
+            tabela=tabela
+        )
+
+@app.route("/estoque")
+@role_required('admin')
+def estoque():
+    dados = estoque_view()
+    return render_template("estoque.html", estoque=dados)
+
+@app.route("/pedido")
+@login_required
+def pedido():
+    return render_template("entrada_pedido.html")
+
+@app.route("/ordens")
+@login_required
+def ordens():
+    dados = ordens_view()
+    return render_template("ordens.html", pedidos=dados)
+
+from datetime import datetime
+
+@app.template_filter('data_br')
+def data_br(valor):
+    if not valor:
+        return '-'
+
+    if isinstance(valor, str):
+        for formato in ("%Y-%m-%d", "%Y-%m-%d %H:%M:%S.%f", "%Y-%m-%d %H:%M:%S"):
+            try:
+                valor = datetime.strptime(valor, formato)
+                break
+            except:
+                continue
+
+    if isinstance(valor, datetime):
+        return valor.strftime('%d/%m/%Y')
+
+    return valor
+
+@app.template_filter('default_br')
+def default_br(valor):
+    return valor if valor not in (None, '', 'None') else '-'
+
+# Simulação de banco
+usuarios = ["Nicolas", "Domingos", "Josemir"]
+
+@app.route("/obter-nomes", methods=["GET"])
+@role_required('admin')
+def obter_nomes():
+    return jsonify(usuarios)
+
+@app.route("/registrar-pedido", methods=["POST"])
+@login_required
+def registrar():
+
+    data = request.json
+
+    qr = data.get("qr")
+    volume = data.get("volume")
+    quem = data.get("quem")
+    quantidade = data.get("quantidade")
+    status = data.get("status")
+
+    if not qr:
+        return jsonify({"msg": "QR não informado"}), 400
+
+    resultado = registrar_pedido(qr, volume, quem, quantidade, status)
+
+    return jsonify(resultado)
